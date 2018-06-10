89d5e385896432c50f5e61311284c2e53eb46bec
Travis CI User <travis@example.org>
2018-06-10 18:29:24 +0000

    [ci skip] update gh-pages

--- build/app/assets.app.js
+++ build/app/assets.app.js
@@ -33229,7 +33229,7 @@ local.stateInit({
             "npm_package_homepage": "https://github.com/npmdoc/node-npmdoc-request",
             "npm_package_name": "npmdoc-request",
             "npm_package_nameLib": "npmdoc_request",
-            "npm_package_version": "0.0.8"
+            "npm_package_version": "2018.6.10"
         }
     }
 });
